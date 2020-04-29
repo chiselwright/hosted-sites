3bcfad8be8cbbdddfe70ca27c579d7ed9436d9d6
Chisel <chisel@chizography.net>
2020-04-29 15:21:34 +0000

    Improve restart-example.sh (now restart-service)

--- restart-example.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-docker-compose build com-theboardingparty \
-	&& docker-compose up --force-recreate -d com-theboardingparty; \
-	docker-compose logs --follow --tail=5
